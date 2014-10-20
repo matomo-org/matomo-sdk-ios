cd654216900529411278331f5e8a93805042df2f
Mattias Levin <mattias.levin@gmail.com>
2014-10-20 20:35:04 +0200

    Added content tracking feature to demo project

---
+++ PiwikTrackeriOSDemo/ContentTrackerViewController.h
@@ -0,0 +1,15 @@
+//
+//  ContentTrackerViewController.h
+//  PiwikTracker
+//
+//  Created by Mattias Levin on 20/10/14.
+//  Copyright (c) 2014 Mattias Levin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContentTrackerViewController : UIViewController
+
+- (IBAction)adInteraction:(id)sender;
+
+@end
---
+++ PiwikTrackeriOSDemo/ContentTrackerViewController.m
@@ -0,0 +1,33 @@
+//
+//  ContentTrackerViewController.m
+//  PiwikTracker
+//
+//  Created by Mattias Levin on 20/10/14.
+//  Copyright (c) 2014 Mattias Levin. All rights reserved.
+//
+
+#import "ContentTrackerViewController.h"
+#import "PiwikTracker.h"
+
+
+@implementation ContentTrackerViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+  
+  [[PiwikTracker sharedInstance] sendContentImpressionWithName:@"DN" piece:@"dn_image.png" taget:@"http://dn.se"];
+  
+}
+
+- (IBAction)adInteraction:(id)sender {
+  
+  [[PiwikTracker sharedInstance] sendContentInteractionWithName:@"DN" piece:@"dn_image.png" taget:@"http://dn.se"];
+
+  // Open link in external web browser
+  [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.dn.se"]];
+  
+}
+
+
+@end
