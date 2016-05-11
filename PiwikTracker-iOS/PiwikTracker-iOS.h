8e02ba86c5ea06c79716a1eace360e43a04ee345
pivotal <wedrive-core@googlegroups.com>
2016-05-11 10:35:55 +0200

    add support for Carthage

---
+++ PiwikTracker-iOS/PiwikTracker-iOS.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for PiwikTracker-iOS.
+FOUNDATION_EXPORT double PiwikTracker_iOSVersionNumber;
+
+//! Project version string for PiwikTracker-iOS.
+FOUNDATION_EXPORT const unsigned char PiwikTracker_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PiwikTracker_iOS/PublicHeader.h>
+
+
