7d82f63e3eac07a2dd7b4e6caa11d71b21931c72
Cornelius Horstmann <site-github@brototyp.de>
2017-01-11 19:44:55 +0100

    added Dispatcher protocol (#119)

---
+++ PiwikTracker/Dispatcher.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+internal protocol Dispatcher {    
+    var userAgent: String? { get }
+    
+    func send(event: Event, success: ()->(), failure: (_ shouldContinue: Bool)->());
+    
+    func send(events: [Event], success: ()->(), failure: (_ shouldContinue: Bool)->());
+}
