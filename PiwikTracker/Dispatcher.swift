9c6fcfb54453a3e04ae1be7a5c8f56359f502b11
Cornelius Horstmann <horstmann@tbointeractive.com>
2017-01-30 21:20:06 +0100

    added a baseurl to the Dispatcher

--- PiwikTracker/Dispatcher.swift
+++ PiwikTracker/Dispatcher.swift
@@ -1,6 +1,9 @@
 import Foundation
 
 public protocol Dispatcher {
+    
+    var baseURL: URL { get }
+    
     var userAgent: String? { get }
         
     func send(events: [Event], success: ()->(), failure: ()->());
