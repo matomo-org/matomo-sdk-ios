76468da540e67b35ee1ae9b0fd9967bbe8361ee2
Cornelius Horstmann <horstmann@tbointeractive.com>
2017-05-19 22:25:37 +0200

    updated example application to use new startNewSession function

--- Example/iOS Example/AppDelegate.swift
+++ Example/iOS Example/AppDelegate.swift
@@ -19,10 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        Tracker.shared?.dispatch()
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        Tracker.shared?.startNewSession()
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
