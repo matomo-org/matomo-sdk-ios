d073c91882557b86e4f505c6d6484517338e3087
Cornelius Horstmann <horstmann@tbointeractive.com>
2017-05-29 21:28:36 +0200

    Fixed the ObjectiveCCompatibilityChecker

--- Example/iOS Example/ObjectiveCCompatibilityChecker.m
+++ Example/iOS Example/ObjectiveCCompatibilityChecker.m
@@ -6,7 +6,7 @@
 
 - (void)check {
     [Tracker configureSharedInstanceWithSiteID:@"5" baseURL:[NSURL URLWithString:@"http://example.com/piwik.php"]];
-    [[Tracker shared] trackWithView:@[@"example"]];
+    [[Tracker shared] trackWithView:@[@"example"] url:nil];
     [[Tracker shared] trackWithEventWithCategory:@"category" action:@"action" name:nil number:nil];
     [[Tracker shared] dispatch];
 }
