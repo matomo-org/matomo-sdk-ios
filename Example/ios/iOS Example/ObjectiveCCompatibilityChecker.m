3401321a880ede3024326811317d74a7b9cc871d
Cornelius Horstmann <horstmann@tbointeractive.com>
2017-10-14 12:32:10 +0200

    Removed the url as required parameter

--- Example/ios/iOS Example/ObjectiveCCompatibilityChecker.m
+++ Example/ios/iOS Example/ObjectiveCCompatibilityChecker.m
@@ -7,9 +7,13 @@
 - (void)check {
     [PiwikTracker configureSharedInstanceWithSiteID:@"5" baseURL:[NSURL URLWithString:@"http://example.com/piwik.php"] userAgent:nil];
     [[PiwikTracker shared] trackWithView:@[@"example"] url:nil];
-    [[PiwikTracker shared] trackWithEventWithCategory:@"category" action:@"action" name:nil number:nil];
+    [[PiwikTracker shared] trackWithEventWithCategory:@"category" action:@"action" name:nil number:nil url:nil];
     [[PiwikTracker shared] dispatch];
     [PiwikTracker shared].logger = [[DefaultLogger alloc] initWithMinLevel:LogLevelVerbose];
+}
+
+- (void)checkDeprecated {
+    [[PiwikTracker shared] trackWithEventWithCategory:@"category" action:@"action" name:nil number:nil];
 }
 
 @end
