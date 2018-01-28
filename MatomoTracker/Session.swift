niksawtschuk/piwik-sdk-ios

Added a suiteName parameter when instantiating a PiwikTracker to have different stores for different trackers.

PiwikTracker/Session.swift
@@ -25,13 +25,14 @@ struct Session {
 }
 
 extension Session {
-    static func current() -> Session {
-        guard let firstVisit = PiwikUserDefaults.standard.firstVisit else {
-            PiwikUserDefaults.standard.firstVisit = Date()
-            return current()
+    static func current(in piwikUserDefaults: PiwikUserDefaults) -> Session {
+        var piwikUserDefaults = piwikUserDefaults
+        guard let firstVisit = piwikUserDefaults.firstVisit else {
+            piwikUserDefaults.firstVisit = Date()
+            return current(in: piwikUserDefaults)
         }
-        let sessionCount = PiwikUserDefaults.standard.totalNumberOfVisits
-        let lastVisit = PiwikUserDefaults.standard.previousVisit ?? Date()
+        let sessionCount = piwikUserDefaults.totalNumberOfVisits
+        let lastVisit = piwikUserDefaults.previousVisit ?? Date()
         return Session(sessionsCount: sessionCount, lastVisit: lastVisit, firstVisit: firstVisit)
     }
 }

matomo-org/matomo-sdk-ios

MatomoTracker/Session.swift
@@ -25,16 +25,17 @@ struct Session {
 }
 
 extension Session {
-    static func current() -> Session {
+    static func current(in matomoUserDefaults: MatomoUserDefaults) -> Session {
         let firstVisit: Date
-        if let existingFirstVisit = MatomoUserDefaults.standard.firstVisit {
+        var matomoUserDefaults = matomoUserDefaults
+        if let existingFirstVisit = matomoUserDefaults.firstVisit {
             firstVisit = existingFirstVisit
         } else {
             firstVisit = Date()
-            MatomoUserDefaults.standard.firstVisit = firstVisit
+            matomoUserDefaults.firstVisit = firstVisit
         }
-        let sessionCount = MatomoUserDefaults.standard.totalNumberOfVisits
-        let lastVisit = MatomoUserDefaults.standard.previousVisit ?? Date()
+        let sessionCount = matomoUserDefaults.totalNumberOfVisits
+        let lastVisit = matomoUserDefaults.previousVisit ?? Date()
         return Session(sessionsCount: sessionCount, lastVisit: lastVisit, firstVisit: firstVisit)
     }
 }