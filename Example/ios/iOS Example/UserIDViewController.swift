541f8e5ba93fc3e478a5c2f206f799fb1274df5d
Cornelius Horstmann <cornelius@brototyp.de>
2018-08-27 11:23:53 +0200

    Added Sample Code

--- Example/ios/iOS Example/UserIDViewController.swift
+++ Example/ios/iOS Example/UserIDViewController.swift
@@ -6,20 +6,30 @@ class UserIDViewController: UIViewController {
     @IBOutlet weak var signinButton: UIButton!
     @IBOutlet weak var signoutButton: UIButton!
     
+    @IBOutlet weak var visitorIdTextField: UITextField!
     @IBAction func signinAction(_ sender: UIButton) {
         if (self.userIDTextField.text != nil) && (self.userIDTextField.text?.count)! > 0 {
             MatomoTracker.shared.userId = self.userIDTextField.text
-            toggleState()
+            updateUserIdState()
         }
     }
     
     @IBAction func signOutAction(_ sender: UIButton) {
         MatomoTracker.shared.userId = nil
-        toggleState()
+        updateUserIdState()
+    }
+    
+    @IBAction func visitorIdTapped(_ sender: Any) {
+        if (self.visitorIdTextField.text != nil) && (self.visitorIdTextField.text?.count)! > 0 {
+            MatomoTracker.shared.forcedVisitorId = self.visitorIdTextField.text
+            updateVisitorIdState()
+        }
     }
     
     override func viewDidLoad() {
-        toggleState()
+        super.viewDidLoad()
+        updateUserIdState()
+        updateVisitorIdState()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -27,11 +37,15 @@ class UserIDViewController: UIViewController {
         MatomoTracker.shared.track(view: ["menu","user id"])
     }
     
-    private func toggleState() {
+    private func updateUserIdState() {
         self.userIDTextField.text = MatomoTracker.shared.userId
         
         self.signinButton.isEnabled = !isVisitorIdValid()
         self.signoutButton.isEnabled = !self.signinButton.isEnabled
+    }
+    
+    private func updateVisitorIdState() {
+        self.visitorIdTextField.text = MatomoTracker.shared.forcedVisitorId
     }
     
     private func isVisitorIdValid() -> Bool {
