91c25849ae2b75ec1a668d24eb56d10b6aba8cdd
Cornelius Horstmann <site-github@brototyp.de>
2018-11-09 15:32:50 +0100

    Removed unused matomo configuration from the example app (#280)

--- Example/ios/iOS Example/ConfigurationViewController.swift
+++ Example/ios/iOS Example/ConfigurationViewController.swift
@@ -3,27 +3,9 @@ import MatomoTracker
 
 class ConfigurationViewController: UIViewController {
     
-    @IBOutlet weak var baseURLTextField: UITextField!
-    @IBOutlet weak var siteIDTextField: UITextField!
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         MatomoTracker.shared.track(view: ["menu","configuration"])
-        
-        baseURLTextField.text = UserDefaults.standard.url(forKey: "piwik-example-baseurl")?.absoluteString
-        siteIDTextField.text = UserDefaults.standard.string(forKey: "piwik-example-siteid")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        guard let baseURLText = baseURLTextField.text,
-            let baseURL = URL(string: baseURLText),
-            let siteID = siteIDTextField.text else { return }
-        UserDefaults.standard.set(baseURL, forKey: "piwik-example-baseurl")
-        UserDefaults.standard.set(siteID, forKey: "piwik-example-siteid")
-        UserDefaults.standard.synchronize()
-        
-//        PiwikTracker.configureSharedInstance(withSiteID: siteID, baseURL: baseURL)
     }
     
     @IBAction func newSessionButtonTapped(_ sender: UIButton) {
