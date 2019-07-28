d3cfd246d419decc24c03af1e82188c9d546bbc3
Eli Gutovsky <github@gutovsky.me>
2019-07-28 20:48:13 +0300

    Added `@escaping` attributes to the Queue protocol's functions. (#304)
    * Fixed warning; redundant access level
    
    * Added @escaping attribute to Queue functions.
    
    * Update changelog
    
    * Prevented the risk of retain cycles.

--- MatomoTracker/Queue.swift
+++ MatomoTracker/Queue.swift
@@ -4,17 +4,17 @@ public protocol Queue {
     
     var eventCount: Int { get }
     
-    mutating func enqueue(events: [Event], completion: (()->())?)
+    mutating func enqueue(events: [Event], completion: (()-> Void)?)
     
     /// Returns the first `limit` events ordered by Event.date
-    func first(limit: Int, completion: (_ items: [Event])->())
+    func first(limit: Int, completion: @escaping (_ items: [Event]) -> Void)
     
     /// Removes the events from the queue
-    mutating func remove(events: [Event], completion: ()->())
+    mutating func remove(events: [Event], completion: @escaping () -> Void)
 }
 
 extension Queue {
-    mutating func enqueue(event: Event, completion: (()->())? = nil) {
+    mutating func enqueue(event: Event, completion: (()->Void)? = nil) {
         enqueue(events: [event], completion: completion)
     }
 }
