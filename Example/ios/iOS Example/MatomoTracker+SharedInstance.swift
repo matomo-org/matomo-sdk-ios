4436e197c3d11437f23124d2e49452eb20763e72
Cornelius Horstmann <cornelius@brototyp.de>
2020-05-23 15:56:25 +0200

    Fixed an issue where device information and operating system sometimes wasn't tracked

--- Example/ios/iOS Example/MatomoTracker+SharedInstance.swift
+++ Example/ios/iOS Example/MatomoTracker+SharedInstance.swift
@@ -6,7 +6,7 @@ extension MatomoTracker {
         let queue = UserDefaultsQueue(UserDefaults.standard, autoSave: true)
         let dispatcher = URLSessionDispatcher(baseURL: URL(string: "https://demo2.matomo.org/piwik.php")!)
         let matomoTracker = MatomoTracker(siteId: "23", queue: queue, dispatcher: dispatcher)
-        matomoTracker.logger = DefaultLogger(minLevel: .info)
+        matomoTracker.logger = DefaultLogger(minLevel: .verbose)
         matomoTracker.migrateFromFourPointFourSharedInstance()
         return matomoTracker
     }()
