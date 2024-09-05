ad513e2b424e5771635d2ee78bb622ffebb6c64c
Cornelius Horstmann <cornelius@brototyp.de>
2024-09-05 10:48:13 +0200

    Fixed sdkVersion property that is reported to the backend

--- MatomoTracker/MatomoTracker.swift
+++ MatomoTracker/MatomoTracker.swift
@@ -466,5 +466,5 @@ extension MatomoTracker {
 
 extension MatomoTracker {
     /// The version of the Matomo SDKs
-    @objc public static let sdkVersion = "7.3"
+    @objc public static let sdkVersion = "7.5"
 }
