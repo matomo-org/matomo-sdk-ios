7b372a14081007091dd82105fa8feb2da9dc1f00
Iggy Drougge <idrougge@mac.com>
2024-09-06 11:08:14 +0200

    Make CustomDimension fields public (#455)

--- MatomoTracker/CustomDimension.swift
+++ MatomoTracker/CustomDimension.swift
@@ -3,10 +3,10 @@ import Foundation
 /// For more information on custom dimensions visit https://piwik.org/docs/custom-dimensions/
 public struct CustomDimension: Codable {
     /// The index of the dimension. A dimension with this index must be setup in the Matomo backend.
-    let index: Int
+    public let index: Int
     
     /// The value you want to set for this dimension.
-    let value: String
+    public let value: String
     
     public init(index: Int, value: String) {
       self.index = index
